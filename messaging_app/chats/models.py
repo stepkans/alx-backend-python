6285343a8b3f812b3b2090d5e76a07eccf477ffd
Stephen Suyanka <stepkans@gmail.com>
2025-06-01 09:03:50 +0300

    Add User, message and conversation Models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+import datetime
 # Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return self.username
+    
+class Conversation(models.Model):   
+    participants = models.ManyToManyField(User, related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.id}"    
+    
+class Message(models.Model):
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="messages_sent"
+    )
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name="messages"
+    )
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender.username} @ {self.sent_at}: {self.message_body[:30]}"    
