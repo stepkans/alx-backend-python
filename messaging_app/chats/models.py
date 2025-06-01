87b3a012593da9a62227d64ba9daaaa327d85b19
Stephen Suyanka <stepkans@gmail.com>
2025-06-01 09:05:56 +0300

    Add uuid to models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-import datetime
-# Create your models here.
+import uuid
+
 
 class User(AbstractUser):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
@@ -12,15 +13,21 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-    
-class Conversation(models.Model):   
+
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
     participants = models.ManyToManyField(User, related_name="conversations")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Conversation {self.id}"    
-    
+        return f"Conversation {self.conversation_id}"
+
+
 class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sender = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="messages_sent"
     )
@@ -31,4 +38,4 @@ class Message(models.Model):
     sent_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.sender.username} @ {self.sent_at}: {self.message_body[:30]}"    
+        return f"{self.sender.username} @ {self.sent_at}: {self.message_body[:30]}"
