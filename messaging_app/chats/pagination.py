686e3bf633533276377075ae6c62fb92632d23e9
Stephen Suyanka <stepkans@gmail.com>
2025-06-07 22:26:58 +0300

    Add pagination

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,14 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class CustomMessagePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,  
+            'previous': self.get_previous_link(),
+            'results': data
+        })
