28fb85b5c1e47997e077fa9ca94ebc09ecb55032
Stephen Suyanka <stepkans@gmail.com>
2025-06-08 16:20:38 +0300

    Add Middlewre

---
+++ Django-Middleware-0x03/chats/middleware.py
@@ -0,0 +1,18 @@
+import logging
+from datetime import datetime
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.logger = logging.getLogger("request_logger")
+        handler = logging.FileHandler("requests.log")
+        formatter = logging.Formatter('%(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else "Anonymous"
+        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
+        self.logger.info(log_message)
+        return self.get_response(request)
