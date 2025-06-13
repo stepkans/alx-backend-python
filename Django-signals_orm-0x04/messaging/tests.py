2e9327e02e41a324e1f1032785088443665a655c
Stephen Suyanka <stepkans@gmail.com>
2025-06-13 13:48:44 +0300

    Add tests to messaging app

--- Django-signals_orm-0x04/messaging/tests.py
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -1,3 +1,19 @@
+# messaging/tests.py
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification
 
-# Create your tests here.
+class MessageSignalTest(TestCase):
+    def setUp(self):
+        self.sender = User.objects.create(username='sender')
+        self.receiver = User.objects.create(username='receiver')
+
+    def test_notification_creation(self):
+        message = Message.objects.create(
+            sender=sender,
+            receiver=self.receiver,
+            content='Hello!'
+        )
+        notification = Notification.objects.filter(user=self.receiver, message=message).first()
+        self.assertIsNotNone(notification)
+        self.assertEqual(notification.is_read, False)
