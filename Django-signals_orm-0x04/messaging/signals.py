6a4900465b504831ca7f1eaaaeb65a747b376358
Stephen Suyanka <stepkans@gmail.com>
2025-06-13 14:16:43 +0300

    Add signals

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
-from .models import Message, Notification
+from django.db.models.signals import post_save, pre_save
+from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
@@ -9,3 +9,16 @@ def create_notification(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+        
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:  # Meaning the message already exists
+        previous = Message.objects.get(pk=instance.pk)
+        if previous.content != instance.content:
+            # Store the previous content in history
+            MessageHistory.objects.create(
+                message=previous,
+                previous_content=previous.content
+            )
+            # Mark message as edited
+            instance.edited = True        
