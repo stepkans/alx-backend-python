1f51c0224085f7fde63fa61d8d14a789f56dba4a
Stephen Suyanka <stepkans@gmail.com>
2025-06-13 13:43:25 +0300

    Add new project Django-signals_orm-0x04

---
+++ Django-signals_orm-0x04/messaging/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender} -> {self.receiver}'
+    
+class Notification(models.Model):
+    user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, related_name='notification', on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} notification for message {self.message}'
